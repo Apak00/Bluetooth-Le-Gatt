885c3eda0c4f2860793dddf27c2150154c966dfb
Apak00 <apakmahmud@gmail.com>
2017-10-05 19:11:19 +0300

    Indoor Positioning

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/Circles/CircleEquation.java
@@ -0,0 +1,21 @@
+package com.example.android.bluetoothlegatt.Circles;
+
+/**
+ * Created by yedek_pc on 14.08.2017.
+ */
+class CircleEquation
+{
+    public double x2;
+    public double x;
+    public double y2;
+    public double y;
+    public double num;
+
+    public CircleEquation(double a, double b, double c, double d, double e) {
+        x2 = a;
+        x = b;
+        y2 = c;
+        y = d;
+        num = e;
+    }
+}
---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/Helpers/NoSquareException.java
@@ -0,0 +1,15 @@
+package com.example.android.bluetoothlegatt.Helpers;
+
+public class NoSquareException extends Exception {
+
+	public NoSquareException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSquareException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
